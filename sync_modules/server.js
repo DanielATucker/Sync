f1d45b5835bf2cbbdfc7a7b2c4d691e7c4f6aacb
Daniel Tucker <DanielATucker@Outlook.com>
2023-01-07 06:54:10 -0500

    T

--- sync_modules/server.js
+++ sync_modules/server.js
@@ -9,27 +9,28 @@ const io = new Server({
 io.on("connection", (socket) => {
   console.log(`${socket.id} Joined the queue`);
 
-  console.log(socket.rooms); // Set { <socket.id> }
-
   socket.join("main");
-
-  console.log(socket.rooms);
   
+  io.of("main").on("connection", (socket) => {
+    io.to("main").emit("message", "Welcome to Main");
+  });
+
   io.to("main").emit("message", "New queue ping:");
+
+  
   ping(socket.id);
+
+  socket.on("pong", (socket_id) => {
+    io.to("main").emit("message",`Socket ${socket_id} is up`);
+  });
 });
 
-io.on("pong", (socket_id) => {
-  io.to("main").emit("message",`Socket ${socket_id} is up`);
-});
+
 
 function ping() {
   io.to("main").emit("ping");
 };
 
-io.of("main").on("connection", (socket) => {
-  io.to("main").emit("message", "Welcome to Main");
-});
 
 io.listen(6200);
 
