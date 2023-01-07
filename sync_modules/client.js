5aea220e25e319c6ad438dde470b530344eb795f
Daniel Tucker <DanielATucker@Outlook.com>
2023-01-07 06:31:31 -0500

    T

--- sync_modules/client.js
+++ sync_modules/client.js
@@ -2,6 +2,10 @@ import { io } from "socket.io-client";
 
 const socket = io("http://100.69.19.3:6200");
 
+socket.on("connect", () => {
+    console.log("Connected to server")  
+});
+
 socket.on("message", (message) => {
     console.log(message)  
 });
