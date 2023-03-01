fc404789f1e07d15a16d53c814b23d4a32d0fb18
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-01 02:00:46 -0500

    T

--- client.js
+++ client.js
@@ -53,10 +53,6 @@ function Start(serverIp) {
             console.log(`Received manifest: ${JSON.stringify(manifest, null, 2)}`);
 
             if (serverList.includes(manifest.server_ip)) {
-                console.log(`Found: ${manifest.server_ip} in serverList`);
-
-                console.log(`server list: ${JSON.stringify(serverList, null, 2)}`);
-
             }
             else {
                 console.log(`Did not find ${manifest.server_ip} in serverList, adding now.`);
